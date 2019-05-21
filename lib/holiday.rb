6d03a887902fc7e18f97c1f0833c25f130bee3b6
Margot Konig <margot.konig@blackrock.com>
2019-05-21 22:12:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,11 +68,15 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-holiday_hash.map do |season,holiday|
-  holiday.map do |holiday,supplies|
-    holiday if supplies.include?("BBQ")
+  bbq_holidays = []
+  holiday_hash.each do |season, holidays|
+    holidays.each do |holiday, supplies|
+      if supplies.includes?("BBQ")
+        bbq_holidays << holiday
+      end
+    end
   end
-end.flatten.compact
+  bbq_holidays
 end
 
 
